c5935434dc90d34ac138db46651edc3e3e4c50dc
b44ken <bradboratto@gmail.com>
2025-01-27 22:45:00 -0500

    some work on crossword

--- vite/src/components/Crossword/CrosswordGame.jsx
+++ vite/src/components/Crossword/CrosswordGame.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 
 export default ({ supabaseClient }) => {
     const [data, setData] = useState(null)
+    const [board, setBoard] = useState(null)
 
     useEffect(() => {
         (async () => {
@@ -12,7 +13,7 @@ export default ({ supabaseClient }) => {
 
             if (error)
                 console.error(error)
-            else if(data.length == 0)
+            else if (data.length == 0)
                 console.error('No data found')
             else
                 setData(data[0].json)
@@ -21,13 +22,52 @@ export default ({ supabaseClient }) => {
 
     if (!data)
         return <div>Loading</div>
-    
-    const svg = data.body[0].board
+
+    const { board: svg, clues } = data.body[0]
+
+    console.log(data)
+
+    const formatClues = (clues, dir) =>
+        clues
+            .filter(c => c.direction == dir)
+            .map(c => 
+                <div key={c.cells}>
+                    <b>({c.label})</b> {c.text[0].plain}
+                </div>)
+
+    const makeInputs = () => {
+        const { width, height } = data.body[0].dimensions
+        const contents = Array(width * height).fill('')
+        const inputs = contents.map((_, i) => 
+            <input key={i} type='text' maxLength={1} className='text-center border-2 border-white m-[-1px]' style={{
+                width: 1/width * 100 + '%',
+                height: 1/height * 100 + '%'
+            }} onChange={e => console.log(e, i)} />)
+        setBoard(contents)
+        return inputs
+    }
+
+    makeInputs()
 
     return (
         <div>
             <h1>Crossword Game</h1>
-            <div className='w-1/2 bg-white' dangerouslySetInnerHTML={{ __html: svg }}></div>
+            <div className='flex flex-row flex-grow justify-center h-1/3'>
+
+                <div className="w-1/4 m-2 inline-block relative">
+                    <div className="bg-white h-full" dangerouslySetInnerHTML={{ __html: svg }}></div>
+                </div>
+                <div className="w-1/4 m-2 inline-block">
+                    <div><b>Down</b></div>
+                    {formatClues(clues, 'Down')}
+                    <br />
+                    <div><b>Across</b></div>
+                    {formatClues(clues, 'Across')}
+                </div>
+                <div className='w-1/4'>
+                    { makeInputs() }
+                </div>
+            </div>
         </div>
     )
 }
