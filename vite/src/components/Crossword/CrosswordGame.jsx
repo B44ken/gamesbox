e4f1af7ee25a5ae65754dda065242305567a851c
b44ken <bradboratto@gmail.com>
2025-01-29 08:43:09 -0500

    more crossword stuff

--- vite/src/components/Crossword/CrosswordGame.jsx
+++ vite/src/components/Crossword/CrosswordGame.jsx
@@ -1,10 +1,11 @@
-import testMini from './mini.json'
-
 import { useEffect, useState } from 'react'
 
 export default ({ supabaseClient }) => {
     const [data, setData] = useState(null)
-    const [board, setBoard] = useState(null)
+    const [cell, setCell] = useState(null)
+    const [start, setStart] = useState(null) // todo
+    const [answers, setAnswers] = useState({})
+
 
     useEffect(() => {
         (async () => {
@@ -23,49 +24,60 @@ export default ({ supabaseClient }) => {
     if (!data)
         return <div>Loading</div>
 
-    const { board: svg, clues } = data.body[0]
+    const { board: svg, clues, cells } = data.body[0]
 
-    console.log(data)
+    const formatClues = () => {
+        const formatClue = (label, clue, highlight) =>
+            <div key={label}>
+                <b>({label})</b> {highlight && <b> {clue} </b> || <span> {clue} </span>}
+            </div>
 
-    const formatClues = (clues, dir) =>
-        clues
-            .filter(c => c.direction == dir)
-            .map(c => 
-                <div key={c.cells}>
-                    <b>({c.label})</b> {c.text[0].plain}
-                </div>)
+        const directions = { Down: [], Across: [] }
+
+        for(let i = 0; i < clues.length; i++) {
+            const highlights = cells[cell]?.clues || []
+            const el = formatClue(clues[i].label, clues[i].text[0].plain, highlights.includes(i))
+
+            directions[clues[i].direction].push(el)
+        }
+
+        return <div>
+            <b>Down</b> {directions.Down}
+            <br />
+            <b>Across</b> {directions.Across}
+        </div>
+    }
 
     const makeInputs = () => {
+        const setAnswer = ({ target }) =>
+            setAnswers({ ...answers, [cell]: target.value.toUpperCase() })
+
         const { width, height } = data.body[0].dimensions
         const contents = Array(width * height).fill('')
         const inputs = contents.map((_, i) => 
-            <input key={i} type='text' maxLength={1} className='text-center border-2 border-white m-[-1px]' style={{
-                width: 1/width * 100 + '%',
-                height: 1/height * 100 + '%'
-            }} onChange={e => console.log(e, i)} />)
-        setBoard(contents)
+            <input key={i} maxLength={1} onSelect={() => setCell(i)} onChange={setAnswer} value={answers[i] || ''}
+                className={`text-center text-black text-xl w-1/${width} aspect-square`} />)
         return inputs
     }
 
-    makeInputs()
+    const didWin = () => {
+        // todo
+    }
+
 
     return (
         <div>
             <h1>Crossword Game</h1>
             <div className='flex flex-row flex-grow justify-center h-1/3'>
 
-                <div className="w-1/4 m-2 inline-block relative">
-                    <div className="bg-white h-full" dangerouslySetInnerHTML={{ __html: svg }}></div>
+                <div className="w-1/3 m-2 inline-block relative">
+                    <div className="bg-white w-full absolute" dangerouslySetInnerHTML={{ __html: svg }}></div>
+                    <div style={{ position: 'absolute' }}>
+                        { makeInputs() }
+                    </div>
                 </div>
-                <div className="w-1/4 m-2 inline-block">
-                    <div><b>Down</b></div>
-                    {formatClues(clues, 'Down')}
-                    <br />
-                    <div><b>Across</b></div>
-                    {formatClues(clues, 'Across')}
-                </div>
-                <div className='w-1/4'>
-                    { makeInputs() }
+                <div className="w-1/3 m-2 inline-block">
+                    { formatClues() }
                 </div>
             </div>
         </div>
